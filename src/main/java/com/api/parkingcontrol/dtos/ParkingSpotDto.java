dd8efd343ae3001b852dc22004d34afbf3edde66
Wilson <wilson-gs@hotmail.com>
2022-05-25 18:02:46 -0300

    implementado controller ParkingController

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,39 @@
+package com.api.parkingcontrol.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParkingSpotDto {
+
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+
+    @NotBlank
+    private String brandCar;
+
+    @NotBlank
+    private String modelCar;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartament;
+
+    @NotBlank
+    private String block;
+}
---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,20 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
